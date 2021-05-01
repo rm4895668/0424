3ed3ceb3f26fcf09dd2c8a28eb0200621fc70b78
LAPTOP-K16A09KQ\ASUS <pa013971@gmail.com>
2021-05-01 17:19:05 +0800

    角色狀態及物體碰撞教學

--- RPG/Assets/FantasyMonster/Skeleton/Character/Player.cs
+++ RPG/Assets/FantasyMonster/Skeleton/Character/Player.cs
@@ -12,8 +12,10 @@ public class Player : MonoBehaviour
     //儲存動畫
     Animator anim;
 
-
-
+    // 0: 可以移動
+    // 1: 攻擊狀態
+    // 2: 被攻擊中
+    public int status = 0;
 
 
 
@@ -22,16 +24,29 @@ public class Player : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        Debug.Log(Checkd(11));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move ();
+    }
+
+    void Move ()
+    {
+
+        if (status != 0) 
+        {
+            return;
+        }
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Debug.Log("左右數值" + h);
-        Debug.Log("左右數值" + v);
+        // Debug.Log("左右數值" + h);
+        // Debug.Log("左右數值" + v);
         //計算移動位置
         velocity = new Vector3(0, 0, v);
         velocity = transform.TransformDirection(velocity);
@@ -51,11 +66,30 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
         {
             anim.SetBool("Attack", true);
+            status = 1;
+        }
+    }
+
+    string Checkd(int cs) 
+    {
+        if (cs> 10)
+        {
+            return "完美";
+        }
+        else
+        {
+            return "不完美";
         }
     }
 
     void AttackEnd()
     {
         anim.SetBool("Attack", false);
+        status = 0;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log("測試碰撞");
     }
 }
